a63d412c00f863947dfae58b7fc69fc8cecf5424
PokeGuys <5060799+PokeGuys@users.noreply.github.com>
2025-03-20 01:23:58 +0800

    chore: add debug log

--- scripts/extract_version.sh
+++ scripts/extract_version.sh
@@ -5,7 +5,6 @@ forced_update=$2
 version_file=$3
 
 latest_version=$(curl -s https://ba.pokeguy.dev/$package_name/version.txt)
-echo -n "$latest_version" > $version_file
 echo "version=$latest_version" >> $GITHUB_OUTPUT
 echo "::notice title=Latest APK Version::$latest_version"
 if [ -z "$latest_version" ]; then
@@ -17,13 +16,17 @@ fi
 if [ "$forced_update" = "true" ]; then
     echo "Forced update. Skipping..."
     echo "skip=false" >> $GITHUB_OUTPUT
+    echo -n "$latest_version" > $version_file
     exit 0
 fi
 
 current_version=$(cat $version_file)
+echo "Current version: $current_version"
 if [ "$current_version" = "$latest_version" ]; then
     echo "No update needed. Skipping..."
     echo "skip=true" >> $GITHUB_OUTPUT
+    echo -n "$latest_version" > $version_file
     exit 0
 fi
 echo "skip=false" >> $GITHUB_OUTPUT
+echo -n "$latest_version" > $version_file
